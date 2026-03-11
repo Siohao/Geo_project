d4cf3c7895a4c3267fe05aa46648fea122238c90
Adrian <stus.adrian@gmail.com>
2026-03-11 19:53:40 +0100

    add python CI

--- geo_project/main.py
+++ geo_project/main.py
@@ -16,30 +16,3 @@ app.add_middleware(
 app.include_router(osm_router)
 app.include_router(owm_router)
 app.include_router(calendar_router)
-
-
-tags_points = {
-    # "tourism": "viewpoint",
-    "natural": "peak"
-}
-
-#way
-tags_routes = {
-    "highway": "path",
-    "sac_scale": None,
-    # "route": "hiking"
-}
-
-#node
-tags_hiking_routes = {
-    "highway": ["path", "steps", "track"]
-}
-
-
-
-    # with open("test_calendar_event.json", "w", encoding="utf-8") as f:
-    # #     ######## .model_dump() -> zmiana z obiektu klasy na dict
-    #     json.dump(data, f, ensure_ascii=False, indent=2)
-    # #     # json.dump(data.model_dump(), f, ensure_ascii=False, indent=2)
-    #     print("Zapisano")
-    # print("koniec")
