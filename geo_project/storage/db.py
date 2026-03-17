3839def61f945e5c005c513963290976db7de6fe
Adrian <stus.adrian@gmail.com>
2026-03-18 00:01:54 +0100

    Another tests with new ci.yml

--- geo_project/storage/db.py
+++ geo_project/storage/db.py
@@ -2,18 +2,23 @@ from psycopg2 import pool
 import os
 from typing import Any
 
-# Minimalny i maksymalny rozmiar puli połączeń
-db_pool = pool.SimpleConnectionPool(
-    1, 10, 
-    host=os.getenv("POSTGRES_HOST", "postgres-db"),
-    port=os.getenv("POSTGRES_PORT",5432),
-    database="geo_project_db",
-    user="admin",
-    password="admin"
-)
+_db_pool = None
+
+def get_pool():
+    global _db_pool
+    if _db_pool is None:
+        _db_pool = pool.SimpleConnectionPool(
+            1, 10, 
+            host=os.getenv("POSTGRES_HOST", "postgres-db"),
+            port=os.getenv("POSTGRES_PORT",5432),
+            database="geo_project_db",
+            user="admin",
+            password="admin"
+        )
+    return _db_pool
 
 def get_conn() -> Any:
-    return db_pool.getconn()
+    return get_pool().getconn()
 
 def release_conn(conn) -> None:
-    db_pool.putconn(conn)
+    get_pool().putconn(conn)
